d919e67564b662d1fc87cab8464ede1c7ddc1753
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-08-29 23:34:53 +0300

    first Question

--- secondAssignment/Program.cs
+++ secondAssignment/Program.cs
@@ -4,6 +4,14 @@ class Program
     static void Main()
     {
 
+        #region first Question
+        int pages = 464;
+        bool isAvailable = true;
+        if(pages>=300&&isAvailable)
+        {
+            Console.WriteLine("You can borrow this book");
+        }
+        #endregion
 
 
 
