0ffd5fd79ee9385a8003f0d436b967a4e35de62d
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-08-29 23:40:48 +0300

    second Question

--- secondAssignment/Program.cs
+++ secondAssignment/Program.cs
@@ -7,13 +7,27 @@ class Program
         #region first Question
         int pages = 464;
         bool isAvailable = true;
-        if(pages>=300&&isAvailable)
+        if(pages>300&&isAvailable)
         {
             Console.WriteLine("You can borrow this book");
         }
         #endregion
 
+        #region second Question
+        string title = "Refactoring";
+        switch (title) {
+            case "Clean Code":
+                Console.WriteLine("Great choice!");
+                break;
+            case "Refactoring":
+                Console.WriteLine("Nice pick!");
+                break;
+            default: Console.WriteLine("Never heard of it");
+                break;
 
+        }
+
+        #endregion
 
     }
 }
